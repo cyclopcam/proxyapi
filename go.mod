65772ec75bb7977e663e08ac5dff81c99dca735c
Ben Harper <rogojin@gmail.com>
2024-09-07 05:44:35 +0200

    Add code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cyclopcam/proxyapi
+
+go 1.22.0
---
+++ messages.go
@@ -0,0 +1,19 @@
+package proxymsg
+
+// Register your server with the proxy
+type RegisterJSON struct {
+	PublicKey string `json:"publicKey"`
+}
+
+// Response to Register
+type RegisterResponseJSON struct {
+	ProxyPublicKey  string `json:"proxyPublicKey"`  // The proxy's public key (eg "ZO0qmRbISuPHSBIoZnC8sSDBkWrxsLxbiNXgGZIhKEE=")
+	ProxyVpnIP      string `json:"proxyVpnIP"`      // The proxy's IP in the VPN (eg "10.6.0.0")
+	ProxyListenPort int    `json:"proxyListenPort"` // The port that the proxy's Wireguard listens on (eg 51820)
+	ServerVpnIP     string `json:"serverVpnIP"`     // Your IP in the VPN (eg "10.7.3.213")
+}
+
+// Remove your server from the proxy
+type RemoveJSON struct {
+	PublicKey string `json:"publicKey"`
+}
