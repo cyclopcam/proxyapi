6594e75424d758fca95e9266c0c7809e8c4e809d
Ben Harper <rogojin@gmail.com>
2024-10-01 07:27:03 +0200

    Add 'network' parameter when registering IPv4 or IPv6

--- messages.go
+++ messages.go
@@ -3,14 +3,15 @@ package proxyapi
 // Register your server with the proxy
 type RegisterJSON struct {
 	PublicKey string `json:"publicKey"`
+	Network   string `json:"network"` // either "IPv4" or "IPv6", will give you a network on the private IPv4 or IPv6 network
 }
 
 // Response to Register
 type RegisterResponseJSON struct {
 	ProxyPublicKey  string `json:"proxyPublicKey"`  // The proxy's public key (eg "ZO0qmRbISuPHSBIoZnC8sSDBkWrxsLxbiNXgGZIhKEE=")
-	ProxyVpnIP      string `json:"proxyVpnIP"`      // The proxy's IP in the VPN (eg "10.6.0.0")
+	ProxyVpnIP      string `json:"proxyVpnIP"`      // The proxy's IP in the VPN (eg "10.6.0.0" or "fdce:c10b:5ca1:1::1", depending on the network choice)
 	ProxyListenPort int    `json:"proxyListenPort"` // The port that the proxy's Wireguard listens on (eg 51820)
-	ServerVpnIP     string `json:"serverVpnIP"`     // Your IP in the VPN (eg "10.7.3.213")
+	ServerVpnIP     string `json:"serverVpnIP"`     // Your IP in the VPN (eg "10.7.3.213" or "fdce:c10b:5ca1:2::13", depending on the network choice)
 }
 
 // Remove your server from the proxy
